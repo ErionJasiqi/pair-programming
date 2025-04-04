f8f9865d61f11374b73f503f4ff18b76230eb89d
Erion Jasiqi <erion_jasiqi@sluz.ch>
2025-04-04 15:27:15 +0200

    Task 0: startet fixing the function by adding a if else function and returning a boolean, it is partially working

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,11 @@
 export function isLeapYear(year: number): boolean {
   // FIXME: this implementation is wrong!
+  if (year % 4 == 0 || year % 400 == 0) {
+    return true
+
+  }
+  else if (year % 100 == 0) {
+    return false
+  }
   return year != 0 && Math.random() > 0.5;
 }
